deb51c044b61e421cc10b3cf6790c71f21a5e7ce
Cremilton Fernandes <cremilton@outlook.com>
2024-07-01 07:30:50 -0300

    Refactoring

--- js/app.js
+++ js/app.js
@@ -1,40 +1,39 @@
-// get username
-let getUsername = document.querySelector(".username");
-// checked on || off
-let statusChecked = document.querySelector(".save");
+(function( win, doc ) {
+  'use strict';
+  let $getUsername = doc.querySelector( '[data-js="username"]' );
+  let $statusChecked = doc.querySelector( '[data-js="save"]' );
 
-// restore username
-let restoreUsername = localStorage.userIFPR;
-function restoreEvent(){
-    if (restoreUsername) {
-        getUsername.value = localStorage.userIFPR;
-        statusChecked.checked = true;
+  let restoreUsername = localStorage.userIFPR;
+  function restoreDataUser() {
+    if ( restoreUsername ) {
+      $getUsername.value = localStorage.userIFPR;
+      $statusChecked.checked = true;
     }
-}
-window.addEventListener("load", restoreEvent);
+  }
+  win.addEventListener( 'load', restoreDataUser );
 
-// save and remove username
-function actiongetUsername() {
-    if(statusChecked.checked){
-        if(getUsername.value === ""){
-            localStorage.userIFPR = "Undefined";
-        }
-        else {
-            localStorage.userIFPR = getUsername.value;
-        }
+  function saveAndDeleteUsername() {
+    if( $statusChecked.checked ) {
+      if( $getUsername.value === '' ) {
+        localStorage.userIFPR = 'Undefined';
+      }
+      else {
+        localStorage.userIFPR = $getUsername.value;
+      }
     } 
-    else{
-        getUsername.value = "";
-        statusChecked.checked = false;
-        localStorage.removeItem("userIFPR");
+    else {
+      $getUsername.value = '';
+      $statusChecked.checked = false;
+      localStorage.removeItem( 'userIFPR' );
     }
-}
-statusChecked.addEventListener("click", actiongetUsername);
+  }
+  $statusChecked.addEventListener( 'click', saveAndDeleteUsername );
 
-// update username
-function updateUser(e) {
-    if(statusChecked.checked){
-        localStorage.userIFPR = getUsername.value;
+  function updateUser( checked ) {
+    if( $statusChecked.checked ) {
+      localStorage.userIFPR = $getUsername.value;
     }
-}
-getUsername.addEventListener("change", updateUser);
+  }
+  $getUsername.addEventListener( 'change', updateUser );
+
+})( window, document );
